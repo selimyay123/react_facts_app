450d8177ffa7457ec61fda829c8845b393d655e1
Selim Yay <selimyay@192.168.1.33>
2024-12-15 18:57:19 +0300

    separated the logic of navbar and the main content

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css'
+import Navbar from './components/Navbar';
+import Main from './components/Main';
 
 function App() {
-  return (<></>)
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+function Navbar() {
+    return <h1>Navbar goes here</h1>
+}
+
+export default Navbar
